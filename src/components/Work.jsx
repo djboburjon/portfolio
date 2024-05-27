cf860676be50b763147cd73cd5aab184e340702e
djboburjon <djboburdev@gmail.com>
2024-05-27 05:35:05 -0700

    sm and md style changed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,21 +22,21 @@ function Work() {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${BuildCon})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center lg:items-center items-end mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
+            <div className="lg:opacity-0 lg:group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wide">
                 Supports Tailwind CSS
               </span>
-              <div className="pt-8 text-center">
+              <div className="lg:pt-8 pt-6 text-center">
                 <a href="https://buildcon-ten.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/djboburjon/buildcon" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -45,21 +45,21 @@ function Work() {
           </div>
           <div
             style={{ backgroundImage: `url(${ElectronicShop})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center lg:items-center items-end mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wide">
+            <div className="lg:opacity-0 lg:group-hover:opacity-100">
+              <span className="text-2xl font-bold lg:text-white text-gray-700 tracking-wide">
                 Website formed Bootstrap
               </span>
-              <div className="pt-8 text-center">
+              <div className="lg:pt-8 pt-6 text-center">
                 <a href="https://electronic-product-shop.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/djboburjon/electronic-shop" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -69,21 +69,21 @@ function Work() {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${HomeSmart})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center lg:items-center items-end mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
+            <div className="lg:opacity-0 lg:group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wide">
                 React JS Application
               </span>
-              <div className="pt-8 text-center">
+              <div className="lg:pt-8 pt-6 text-center">
                 <a href="https://vite-homestart.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/djboburjon/tailwind_vite" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -92,21 +92,21 @@ function Work() {
           </div>
           <div
             style={{ backgroundImage: `url(${Mixel})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center lg:items-center items-end mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wide">
+            <div className="lg:opacity-0 lg:group-hover:opacity-100">
+              <span className="text-2xl font-bold lg:text-white text-gray-700 tracking-wide">
                 React JS Application
               </span>
-              <div className="pt-8 text-center">
+              <div className="lg:pt-8 pt-6 text-center">
                 <a href="https://mixel-uz.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/djboburjon/mixel.uz" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -116,21 +116,21 @@ function Work() {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${News})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center lg:items-center items-end mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wide">
+            <div className="lg:opacity-0 lg:group-hover:opacity-100">
+              <span className="text-2xl font-bold lg:text-white text-gray-700 tracking-wide">
                 An Information Website
               </span>
-              <div className="pt-8 text-center">
+              <div className="lg:pt-8 pt-6 text-center">
                 <a href="https://namanganliklar24-news.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/djboburjon/namanganliklar24" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -139,21 +139,21 @@ function Work() {
           </div>
           <div
             style={{ backgroundImage: `url(${Rolling})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center lg:items-center items-end mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wide">
+            <div className="lg:opacity-0 lg:group-hover:opacity-100">
+              <span className="text-2xl font-bold lg:text-white text-gray-700 tracking-wide">
                 Supports Tailwind CSS
               </span>
-              <div className="pt-8 text-center">
+              <div className="lg:pt-8 pt-6 text-center">
                 <a href="https://tailwind-rolling.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="https://github.com/djboburjon/tailwind_rolling" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-3 py-1 lg:px-4 lg:py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
